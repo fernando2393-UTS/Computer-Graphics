0d528cac4b7546ed26fc3b7e931475fdbe6ee89b
fernando2393 <garciasanzfernando2393@gmail.com>
2018-05-31 16:25:35 +1000

    Key down functio

--- Final Project/js/keyDown.js
+++ Final Project/js/keyDown.js
@@ -11,4 +11,10 @@ document.onkeydown = function(evt) {
     if(evt.keyCode == 83){ // Pressing key S
         camera.position.z += 3;
     }
+    if (evt.keyCode == 27) { // Pressing ESC
+        if (selected_obj) {
+          selected_obj.material.color = selected_obj_color;
+          selected_obj = null;
+        }
+      }
 };
