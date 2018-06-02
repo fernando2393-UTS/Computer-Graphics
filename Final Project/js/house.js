c74c486037474e53e52e8fb84dcee1fc24c9d5f0
ebnera <alex__ebner@hotmail.com>
2018-06-02 15:16:16 +1000

    new plys

--- Final Project/js/house.js
+++ Final Project/js/house.js
@@ -232,15 +232,15 @@ var bedroom_light;
 var bathroom_light;
 
 function addLighting() {
-  livingroom_light = new THREE.PointLight(new THREE.Color(1, 1, 1), (1), (150) ,2);
-  livingroom_light.position.set(5, 10, 3);
+  livingroom_light = new THREE.PointLight(new THREE.Color(1, 1, 1), (0.2));
+  livingroom_light.position.set(5, 20, 3);
   //scene.add(livingroom_light);
 
-  bedroom_light = new THREE.PointLight(new THREE.Color(1, 1, 1), (1), (150) ,2);
-  bedroom_light.position.set(-10, 10, 0);
+  bedroom_light = new THREE.PointLight(new THREE.Color(1, 1, 1), (2));
+  bedroom_light.position.set(-10, 20, 0);
   //scene.add(bedroom_light);
 
-  bathroom_light = new THREE.PointLight(new THREE.Color(1, 1, 1), (1), (150) ,2);
-  bathroom_light.position.set(-3, 10, 43);
- // scene.add(bathroom_light);
+  bathroom_light = new THREE.PointLight(new THREE.Color(1, 1, 1), (2));
+  bathroom_light.position.set(-3, 20, 43);
+  //scene.add(bathroom_light);
 }
