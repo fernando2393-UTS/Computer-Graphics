f6ea30032f865b7f588f728b497f91dfea13c19a
Hendrik Hangen <hendrik.hangen@studium.uni-hamburg.de>
2018-06-06 11:01:34 +1000

    update§

--- Final Project/js/gui.js
+++ Final Project/js/gui.js
@@ -10,8 +10,8 @@ var FizzyText = function () {
   this.bedroom_light = bedroom_light.intensity;
   this.bathroom_light = bathroom_light.intensity;
   this.turn_off_lights = false;
-  this.floor_texture = '/img/txtr_floor.jpg';
-  this.wall_texture = '/img/txtr_brick_brown.jpg';
+  this.floor_texture = 'img/txtr_floor.jpg';
+  this.wall_texture = 'img/txtr_brick_brown.jpg';
   this.camera = 0
 };
 
@@ -45,19 +45,19 @@ function buildGui() {
     }
   });
   gui.add(params, 'floor_texture', {
-    'Darkbrown Wood': '/img/txtr_floor.jpg',
-    'Brown Wood': '/img/txtr_wood2.jpg',
-    'Grey Wood': '/img/txtr_wood1.jpg',
-    'Stone Floor': '/img/txtr_floor_stone.jpg'
+    'Darkbrown Wood': 'img/txtr_floor.jpg',
+    'Brown Wood': 'img/txtr_wood2.jpg',
+    'Grey Wood': 'img/txtr_wood1.jpg',
+    'Stone Floor': 'img/txtr_floor_stone.jpg'
   }).onChange(function (val) {
     updateTexture(floor, val, WallSideEnum.top, 10);
   });
   gui.add(params, 'wall_texture', {
-    'Brown Bricks': '/img/txtr_brick_brown.jpg',
-    'Sand Wall': '/img/txtr_wall_sand.jpg',
-    'Stone Wall': '/img/txtr_stone.png',
-    'Stone Wall 2': '/img/txtr_stone.jpg',
-    'Wooden Wall': '/img/txtr_wood_brown.jpg'
+    'Brown Bricks': 'img/txtr_brick_brown.jpg',
+    'Sand Wall': 'img/txtr_wall_sand.jpg',
+    'Stone Wall': 'img/txtr_stone.png',
+    'Stone Wall 2': 'img/txtr_stone.jpg',
+    'Wooden Wall': 'img/txtr_wood_brown.jpg'
   }).onChange(function (val) {
     updateTexture(wall_bottom_out, val, WallSideEnum.front, 8);
     updateTexture(wall_top_out, val, WallSideEnum.back, 8);
